77813b16e87c7d7c950ed9ffcc3ba44c03d812e6
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-09-02 18:52:45 +0530

    Show Route

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/livinn";
@@ -17,9 +21,28 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Hell I'am root");
 });
+
+
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allLinstings = await Listing.find({});
+  res.render("listings/index.ejs", { allLinstings });
+});
+
+// Show Route
+
+app.get("/listings/:id",async (req, res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", {listing})
+})
+
 
 app.listen("8080", () => {
   console.log("App is listening on port 8080");
